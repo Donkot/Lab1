fce7c081864b2c3689d35067e33012fcc157abf8
Donkot <31614663+Donkot@users.noreply.github.com>
2018-01-11 21:57:17 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,42 +1,25 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
   FILE *fp;
-  char ch;
-  char it[2];
-  char chr[256];
-  char fl[4];
+  char ch[256];
+  int num;
+  float f;
   if((fp=fopen(argv[1] /*"data.dat"*/,"r"))==NULL) {
     printf("Невозможно открыть файл.\n");
     exit(1);
-  }
-  while((ch=fgetc(fp)) != EOF) {
-      while((ch=fgetc(fp)) != EOF) {
-      it[0] = ch;
-      int i = 0;
-      while(((ch=fgetc(fp)) != EOF) && (i < 1)) {
-        it[i+1] = ch;
-        i++;
-      }
-      short resulti = *(short*)&it;
-      i = 0;
-      while(((ch=fgetc(fp)) != EOF) && (i < 255)) {
-        chr[i] = ch;
-        i++;
-      }
-      i = 0;
-      while(((ch=fgetc(fp)) != EOF) && (i < 4)) {
-        fl[i] = ch;
-        i++;
-      }
-      float resultf = *(float*)&fl;
-
-      printf("%d\t%s\t%f\n", resulti, chr, resultf);
-    }
-  }
-  fclose(fp);
-  getchar();
-  return 0;
+}
+        while(!feof(fp)){
+            fread(&num, sizeof(int), 1, fp);
+            for( int i = 0; i < 256; i++){
+                fread(ch + i, 1, 1, fp);
+            }
+            fread(&f, sizeof(f), 1, fp);
+            printf("%2d | %s | %f\n", num, ch, f);
+        }
+    fclose(fp);
+    getchar();
+return 0;
 }
